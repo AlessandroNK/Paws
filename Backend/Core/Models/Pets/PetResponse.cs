8cf0df083539138199ae6562ed28fdfc252e0e31
Yomar Posada <yomarposada@gmail.com>
2026-05-30 17:08:17 -0500

    add PetResponse model to handle pet data representation; include properties for id, name, species, breed, and timestamps

---
+++ Backend/Core/Models/Pets/PetResponse.cs
@@ -0,0 +1,65 @@
+using Backend.Core.Models.Enums;
+
+namespace Backend.Core.Models.Pets;
+
+public class PetResponse
+{
+    //                                                                                                Private Properties
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    //                                                                                                 Public Properties
+    // -----------------------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// The id of the pet in the database
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The name of the pet.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The species of the pet
+    /// </summary>
+    public PetSpecies Species { get; set; } = PetSpecies.Other;
+
+    /// <summary>
+    /// The breed of the pet. it will depend on
+    /// </summary>
+    public string Breed { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The date and time when the pet was created. It is used to track when the pet was created and to sort pets by
+    /// creation date.
+    /// </summary>
+    public DateTime CreatedAt { get; init; }
+
+    /// <summary>
+    /// The date and time when the pet was last updated. It is used to track when the pet was last updated and to sort
+    /// pets
+    /// </summary>
+    public DateTime UpdatedAt { get; set; }
+
+
+    //                                                                                                         Operators
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    //                                                                                                            Events
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    //                                                                                                      Constructors
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    //                                                                                                   Private Methods
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    //                                                                                                    Public Methods
+    // -----------------------------------------------------------------------------------------------------------------
+
+}
